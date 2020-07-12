d63b145ef1221c01ec1ce15f7253079397bc7852
Levoment <30224138+Levoment@users.noreply.github.com>
2020-07-12 13:01:06 -0500

    Modified when to load the script. Modified the content script to appropiately apply the removal of overlays when videos are loaded from the home page, search page, or directly loaded from the url bar. Added icons for the extension that will actually show on Chromium based browsers.

--- content/YouTubeOverlayRemoverContentScript.js
+++ content/YouTubeOverlayRemoverContentScript.js
@@ -2,6 +2,77 @@
 console.info("YouTube Overlay Remover Loaded");
 var currentStatus;
 
+// Variable holding the current url
+var currentURL = document.URL;
+
+// Set a timeout variable in case the movie player doesn't load
+var timeout;
+// Set the target element to scan for changes
+var targetNode = document.getElementById('movie_player');
+
+// Primary target node
+var primaryNode = document.getElementById('primary');
+// Search page target node
+var pageManagerTargetNode = document.getElementById('page-manager');
+
+// Set the MutationObserver options
+var primaryNodeObserverOptions = {
+    // False to improve performance
+    childList: false,
+    // Monitor the class changes
+    attributeFilter: ["class"],
+    // Monitor nodes within the target node
+    subtree: true
+}
+
+// Create the MutationObserver for the primary node
+var primaryNodeObserver = new MutationObserver(primaryNodeMutationObserverCallback);
+// Create the mutation observer for the page manager node
+var pageManagerObserver = new MutationObserver(primaryNodeMutationObserverCallback);
+
+function primaryNodeMutationObserverCallback(mutationList, mutationObserver) {
+    // Get the current URL
+    currentURL = document.URL;
+    const regex = RegExp(".youtube.com\/watch*");
+    if (regex.test(currentURL)) {
+        // Check if the target element was succesfully gathered
+        if (typeof targetNode !== 'undefined' && targetNode != null) {
+            // Get the current status of the extension
+            getCurrentStatus();
+        } else {
+            // The element was not gathered. Poll to gather it
+            timeout = setTimeout(function () {
+                targetNode = document.getElementById('movie_player');
+                // If the target element was gathered
+                if (typeof targetNode !== 'undefined' && targetNode != null) {
+                    // Get the current status of the extension
+                    getCurrentStatus();
+                }
+            }, 3000);
+        }
+    }
+}
+
+primaryNodeObserver.observe(primaryNode, primaryNodeObserverOptions);
+pageManagerObserver.observe(pageManagerTargetNode, primaryNodeObserverOptions);
+
+function getCurrentStatus() {
+    // If the timeout was used
+    if (typeof targetNode !== 'undefined' && targetNode != null) {
+        // Clear it out
+        clearTimeout(timeout);
+    }
+
+    // Depending on the type of browser the extension is running on, ask the background script for the extension's current state
+    if (typeof browser !== 'undefined' && (typeof browser.runtime !== 'undefined' && browser.runtime != null)) {
+        // Ask for the extension current state and set where to listen for the response
+        browser.runtime.sendMessage({ "operation": "getCurrentState" }, handleGetStatusResponse);
+    } else if (typeof chrome !== 'undefined' && (typeof chrome.runtime !== 'undefined' && chrome.runtime != null)) {
+        // Ask for the extension current state and set where to listen for the response
+        chrome.runtime.sendMessage({ "operation": "getCurrentState" }, handleGetStatusResponse);
+    }
+}
+
 if (typeof browser !== 'undefined' && (typeof browser.runtime !== 'undefined' && browser.runtime != null)) {
     // Set a listener for when messages are received from the background script
     browser.runtime.onMessage.addListener(handleReceivedMessages);
@@ -23,9 +94,6 @@ function handleReceivedMessages(message, sender, sendResponse) {
     }
 }
 
-// Set the target element to scan for changes
-var targetNode = document.getElementById('movie_player');
-
 // Set the MutationObserver options
 var observerOptions = {
     // False to improve performance
@@ -38,14 +106,6 @@ var observerOptions = {
 
 // Create the MutationObserver
 var observer = new MutationObserver(mutationObserverCallback);
-
-if (typeof browser !== 'undefined' && (typeof browser.runtime !== 'undefined' && browser.runtime != null)) {
-    // Set a listener for when messages are received from the background script
-    browser.runtime.sendMessage({ "operation": "getCurrentState" }, handleGetStatusResponse);
-} else if (typeof chrome !== 'undefined' && (typeof chrome.runtime !== 'undefined' && chrome.runtime != null)) {
-    // Set a listener for when messages are received from the background script
-    chrome.runtime.sendMessage({ "operation": "getCurrentState" }, handleGetStatusResponse);
-}
 
 // Function to handle responses from the background script
 function handleGetStatusResponse(message) {
@@ -189,5 +249,4 @@ function mutationObserverCallback(mutationList, mutationObserver) {
             }
         }
     }
-
 }
